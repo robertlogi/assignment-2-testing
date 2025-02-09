d65bb8b9dcaf62a3de0d8f28eb24dbf04f595c4c
Róbert Logi Jónsson <robertlogi15@gmail.com>
2025-02-09 22:07:25 +0000

    Update dateUtils.ts
    Type hints have been updated to restrict string flexibility in parameters, simplifying their purpose.

--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -5,18 +5,18 @@ export function getCurrentYear(): number {
   return moment().year();
 }
 
-export function add(date: Date | string, number: number, type: moment.unitOfTime.DurationConstructor = DATE_UNIT_TYPES.DAYS): Date {
+export function add(date: Date, number: number, type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS): Date {
   return moment(date).add(number, type).toDate();
 }
 
-export function isWithinRange(date: Date | string, from: Date | string, to: Date | string): boolean {
+export function isWithinRange(date: Date, from: Date, to: Date): boolean {
   return moment(date).isBetween(from, to);
 }
 
-export function isDateBefore(date: Date | string, compareDate: Date | string): boolean {
+export function isDateBefore(date: Date, compareDate: Date): boolean {
   return moment(date).isBefore(compareDate);
 }
 
-export function isSameDay(date: Date | string, compareDate: Date | string): boolean {
+export function isSameDay(date: Date, compareDate: Date): boolean {
   return moment(date).isSame(compareDate, 'day');
 }
